e3af12a305a5ac56ffe92f5ab07aa8d8161bd363
Dmitriy <volozhinov.ds@phystech.edu>
2024-03-12 06:23:12 +0300

    Added: EOF processing, individual pattern for each keyword

--- HW1/Logic.cpp
+++ HW1/Logic.cpp
@@ -1,13 +1,14 @@
-#include <stdlib.h>
+#include "stdlib.h"
 
 void error(int code) {
     switch (code)
     {
     case 1:
-        std::cout << "SYNTAX ERROR";
+        std::cout << "LEXICAL ERROR";
         break;
     case 2:
         std::cout << "UNCLOSED COMMENT";
+        break;
     default:
         std::cout << "UNKNOWN ERROR CODE";
         break;
@@ -15,4 +16,43 @@ void error(int code) {
 
     std::cout << "; LINE: " << yylineno << std::endl;
     exit(0);
+}
+
+void operation_call(char OP) {
+    std::cout << "Token class: OP,       Token value: ";
+    switch (OP)
+    {
+    case '+':
+        std::cout << "ADD";
+        break;
+    case '-':
+        std::cout << "SUB";
+        break;
+    case '*':
+        std::cout << "MUL";
+        break;
+    case '/':
+        std::cout << "DIV";
+        break;
+    case '=':
+        std::cout << "ASSIGN";
+        break;
+    }
+    std::cout << std::endl;
+}
+
+void keyword_call(std::string word) {
+    std::cout << "Token class: KEYWORD,  Token value: " << word << std::endl;
+}
+
+void number_call() {
+    std::cout << "Token class: NUMBER,   Token value: " << yytext << std::endl;
+}
+
+void type_call() {
+    std::cout << "Token class: TYPE_ID,  Token value: " << yytext << std::endl;
+}
+
+void var_call() {
+    std::cout << "Token class: VARIABLE, Token value: " << yytext << std::endl;
 }
